7c32aa635c5e66c03016aa19fbb229b3188756c6
Magnus <michal.j.olender@gmail.com>
2022-08-03 22:59:11 +0200

    completed 3.8 and 3.9 3.10 in progress

---
+++ 3/diy/3-10_every_function.py
@@ -0,0 +1,17 @@
+locations = ['japan', 'usa', 'south korea', 'china']
+
+print(locations)
+
+locations.reverse()
+
+print(locations)
+
+locations.sort(reverse=True)
+
+print(locations)
+
+locations.sort()
+
+print(locations)
+
+print(len(locations))
---
+++ 3/diy/3-8_seeing_the_world.py
@@ -0,0 +1,23 @@
+locations = ['japan', 'maroko', 'usa', 'china', 'south korea']
+
+print(locations)
+
+print(sorted(locations, reverse=True))
+
+print(locations)
+
+locations.reverse()
+
+print(locations)
+
+locations.reverse()
+
+print(locations)
+
+locations.sort(reverse=True)
+
+print(locations)
+
+locations.sort()
+
+print(locations)
