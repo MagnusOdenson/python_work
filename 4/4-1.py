9e0c59d97283ccf3b1d3f919d77c476bcc4383be
MagnusOdenson <michal.j.olender@gmail.com>
2022-08-08 02:35:58 +0200

    finish chapter 3, start chapter 4 stop at page 96

---
+++ 4/4-1.py
@@ -0,0 +1,4 @@
+pizzas = ['meat lovers','margharita','pepperoni']
+for pizza in pizzas:
+    print(f"{pizza.title()}, is the best pizza I've ever eaten.")
+print("\nPizza is the greatest food ever created.")
---
+++ 4/4-2.py
@@ -0,0 +1,4 @@
+animals = ['cat','dog','rat']
+for animal in animals:
+    print(f"A {animal}, would make a marvelous pet.")
+print("\nAll of these animals have four legs.")
