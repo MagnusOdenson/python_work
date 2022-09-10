a9d930d172656a8c884aeb3b607fbad69e2f6360
MagnusOdenson <michal.j.olender@gmail.com>
2022-09-10 12:51:00 +0200

    add toppings.py

--- 5/toppings.py
+++ 5/toppings.py
@@ -1,13 +1,14 @@
-requested_topping = ['mushrooms', 'extra cheese']
+from zoneinfo import available_timezones
 
-# if requested_topping != 'anchovies':
-#     print("Hold the anchovies!")
 
-if 'mushrooms' in requested_topping:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_topping:
-    print("Adding pepperoni")
-if 'extra cheese' in requested_topping:
-    print("Adding extra cheese.")
+available_toppings = ['mushrooms', 'olives', 'green peppers', 'pepperoni', 'pineapple', 'extra cheese']
+
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry, we don't have {requested_topping}.")
 
 print("\nFinished making your pizza!")
