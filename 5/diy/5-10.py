040e560b50e0c3a58fe5e7026d11b5f8c7c995c0
MagnusOdenson <michal.j.olender@gmail.com>
2022-09-17 16:32:24 +0200

    add finished exercises from chapter 5

--- 5/diy/5-10.py
+++ 5/diy/5-10.py
@@ -1,3 +1,12 @@
-current_users = []
+current_users = ['marcin', 'wiesiek','marta','jacek','seba']
 
-new_users = []
+new_users = ['wiesiek','pajonk','jacek','anna','sylwek']
+
+current_users_lower = [user.lower() for user in current_users]
+
+for new_user in new_users:
+    if new_user.lower() in current_users_lower:
+        print("Sorry " + new_user + ", that name is taken")
+    else:
+        print("Great, " + new_user + " is still available.")
+
